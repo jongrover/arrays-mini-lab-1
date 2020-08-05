df699bebeffeadef7aeacdc7692143949127323a
jongrover <hello@jonathangrover.com>
2020-08-05 19:07:32 -0400

    add solution

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var animals;
 
 // Write your solution code below here using the variables declared above.
 
+animals = ['Cat', 'Dog', 'Bear'];
 
+animals[2] = 'Lion';
 
 // Leave this portion of code alone to make testing work.
 
